c4a4494bb3188a74811913d7e048d36448242a8f
BlobbyDiscord <44066266+BlobbyDiscord@users.noreply.github.com>
2019-09-18 08:18:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-// Create a new webhook
-const hook = new Discord.WebhookClient('webhook id', 'webhook token');
  
 
 client.on('ready', () => {
@@ -24,17 +22,6 @@ client.on('message', message => {
 
 });
 
-
-client.on('message', message => {
-    // If the message is '!rip'
-    if (message.content === '.logo') {
-        // Create the attachment using Attachment
-        const attachment = new Attachment('https://cdn.glitch.com/5df14e9f-5821-4dac-89c0-f460fc156aa9%2F1567935115820.png?v=1568297795770');
-        // Send the attachment in the message channel
-        message.channel.send(attachment);
-    }
-});
-// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find(ch => ch.name === 'members');
